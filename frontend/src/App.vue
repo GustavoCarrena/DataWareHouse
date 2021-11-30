f7c28c50441dfe7f6d754a3ca0e28463e8d91329
porotogus <gustavocarrena@outlook.com>
2021-11-29 21:19:26 -0300

    pendiente validaciones back

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,8 +4,52 @@
   </div>
 </template>
 <style>
+
 body {
-  background: rgb(185, 185, 185);
+    margin: 0 ;
+    padding: 0 ;
+    display: flex ;
+    justify-content: center;
+    align-items: center ;
+    flex-flow: column nowrap;
+    height: auto ;
+    background-color: #f7f6f6;
+}
+
+* {
+    box-sizing: border-box;
+    
+}
+
+#app{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+}
+
+.container{
+  width: 100%;
+}
+
+thead{
+  text-align: center;
+}
+
+td{
+  text-align: center;
+}
+
+.mod{
+  margin: 10px 5px 0 0 !important;
+
+}
+.lab{
+  margin: 2px 2px 0 0;
+  color: rgb(6, 6, 153);
+}
+
+.input{
+  margin-top: 3px !important;
 }
 
 </style>
--- frontend/src/views/Employee.vue
+++ frontend/src/views/Employee.vue
@@ -1,163 +1,337 @@
 <template>
-  <div class="row p-5">
-    <h1>Employee</h1>
-    <b-table 
-      ref="selectableTable"
-      striped 
-      hover 
-      selectable 
-      select-mode="multi"
-      :items="employees" 
-      :fields="fields"      
-      @row-selected="onRowSelected"
-    >
-      <template #cell(status)="{ rowSelected }">
-          <input type="checkbox" :checked="rowSelected" disabled>
-      </template>
-      <template #cell(actions)="row">
-          <b-button size="sm" class="col-3 mr-2" @click="showEdit(row.item)">Editar</b-button>
-      </template>
-    </b-table>
-    <div class="row">
-      <b-button size="sm" class="col-3 mr-2" @click="clearSelected()">Borrar Seleccion</b-button>
-      <b-button size="sm" class="col-3 mr-2" @click="deleteEmployees()">Eliminar Empleados </b-button>
-      <b-button size="sm" class="col-3" @click="showEdit({}, 'create' )">Crear Empleado </b-button>
+  <div id="container" class="row p-6 container">
+    
+    <h1 class="row col-5">Gestion de Usuarios</h1>
+    
+    <div class="btnstop">
+    <b-button size="sm" class="col-3 mr-2 btnppal" variant="success" @click="showEdit({}, 'create')">Alta de Usuario</b-button>
     </div>
-    <b-modal id="editModal" title="Editar Empleado" hide-footer>
-        <p class="my-4">{{employeeSelected.firstname}} {{employeeSelected.lastname}}</p>
-        <div>
-          <input type="text" v-model="employeeSelected.firstname" class="form-control" placeholder="Nombre">
-          <input type="text" v-model="employeeSelected.lastname" class="form-control mt-3" placeholder="Apellido">
-          <input type="text" v-model="employeeSelected.email" class="form-control mt-3" placeholder="Email">
-          <b-form-select class="mt-3" v-model="employeeSelected.role_id" :options="[{ value: 'USER', text: 'Usuario' },{ value: 'ADMI', text: 'administrador' },]"></b-form-select>
-          <input type="text" v-model="employeeSelected.user_pass" class="form-control mt-3" placeholder="Passwords">
-          <b-button @click="$bvModal.hide('editModal')" class="mt-3 mr-4">Cancelar</b-button>
-          <b-button v-if="modalMode === 'edit'" @click="updateEmployee()" class="mt-3" variant="success">Actualizar</b-button>
-          <b-button v-else @click="createEmployee()" class="mt-3" variant="success">crear</b-button>
-        </div>
+    
+    <b-table bordered ref="selectableTable" hover selectable select-mode="multi" responsive="true" sticky-header="55vh" 
+    :items="employees"
+    :fields="fields"
+      @row-selected="onRowSelected">
+        <template #cell(status)="{ rowSelected }">
+          <input name ="checkbox" type="checkbox" :checked="rowSelected" disabled />
+        </template>
+        <template #cell(actions)="row">
+          <b-button
+            variant="warning"
+            size="sm"
+            class="col-8 mr-2"
+            @click="showEdit(row.item)">Editar</b-button>
+        </template>
+    </b-table>
+    
+  
+    <b-modal id="deleteInfo">Registro Eliminado Exitosamente</b-modal>
+          
+          <div class="btnCancelSel">
+          <b-button size="sm" class="col-3 mr-2 btnppal" variant="danger" @click="showMsgBoxOne">Eliminar Usuarios Seleccionados</b-button>
+
+        <b-button size="sm" class="col-3 mr-2 btnppal " variant="outline-danger" @click="clearSelected()">Cancelar Seleccion Realizada</b-button>
+</div>
+
+    <b-modal  class="editModal" id="editModal" title="Ingreso de Datos" hide-footer>
+      <form>
+        
+        <label class="lab" for="name" >* <strong>Nombre</strong></label>
+        
+      
+        <input
+            name="name"
+            type="text"
+            v-model="employeeSelected.firstname"
+            class="form-control input"
+        />
+        
+       
+       
+        <label class="lab" for="lastname">* <strong>Apellido</strong></label>
+          <input
+            name="lastname"
+            type="text"
+            v-model="employeeSelected.lastname"
+            class="form-control mt-3 input"
+          />
+
+        <label class="lab" for="email">* <strong>Correo Electronico</strong></label>
+        <small><i>  (Formato requerido : xxx@xxx.xxx)</i></small>
+        <input
+          name="email"
+          type="text"
+          v-model="employeeSelected.email"
+          class="form-control mt-3 input"
+        />
+        
+        <label class="lab" for="role">* <strong>Permisos</strong></label>
+        <b-form-select
+          name="role"
+          class="mt-3 input"
+          v-model="employeeSelected.role_id"
+          :options="[
+            { value: 'USER', text: 'Usuario' },
+            { value: 'ADMI', text: 'Usuario administrador' },
+          ]"
+        ></b-form-select>
+        <label class="lab" for="pass">* <strong>Contraseña</strong></label>
+        <small><i>  (minimo 6 caracteres)</i></small>
+        <input
+          name="pass"
+          type="text"
+          v-model="employeeSelected.user_pass"
+          class="form-control mt-3 input"
+          
+        />
+          <label class="lab" for="passrep">* <strong>Repetir Contraseña</strong></label>
+          <small><i>  (debe coincidir con la contraseña)</i></small>
+          <input
+          name="passrep"
+          type="text"
+          v-model="employeeSelected.user_passrep"
+          class="form-control mt-3 input"
+          
+        />
+        <b-button @click="$bvModal.hide('editModal')" class="mt-3 mr-4 mod">Cancelar</b-button>
+        <b-button
+          v-if="modalMode === 'edit'"
+          @click="updateEmployee()"
+          class="mt-3 mod"
+          variant="success"
+          >Actualizar</b-button
+        >
+        <b-button
+          v-else
+          @click="createEmployee()"
+          
+          class="mt-3 mod"
+          variant="success"
+          >crear</b-button
+        >
+        <small><i> (*) Campos obligatorios</i></small>
+      </form>
     </b-modal>
+    
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Employee',
-  data(){
-    return{
+  name: "Employee",
+
+  
+  data() {
+    return {
+      
+      
+      boxOne: '',
+      
       employees: [],
-      fields:[
+      
+      fields: [
         {
           key: "status",
           label: "",
         },
         {
           key: "id",
-          sortable: true
+          sortable: true,
         },
         {
           key: "firstname",
           sortable: true,
-          label: "Nombre"
+          label: "Nombre",
         },
         {
           key: "lastname",
           sortable: true,
-          label: "Apellido"
+          label: "Apellido",
         },
         {
           key: "email",
           sortable: true,
-          label: "Correo"
+          label: "Correo",
         },
+
         {
           key: "role_description",
           sortable: true,
-          label: "Permisos"
+          label: "Permisos",
         },
         {
           key: "actions",
           sortable: false,
-          label: "Acciones"
-        }
+          label: "Acciones",
+        },
       ],
       selected: [],
       employeeSelected: {},
-      modalMode:'edit'
-    }
+      modalMode: "edit",
+    };
   },
-  mounted(){
+  
+
+  
+  mounted() {
     this.getEmployees();
   },
-  methods: {
-    getEmployees() {
-      const url = 'http://localhost:3000/employees/employeesData'
-      fetch(url)
-          .then (response =>  response.json())
-          .then(data => this.employees = data.response)
-          .catch(error => error);
-    },
-    showEdit(employee, mode = 'edit'){
-      this.modalMode = mode;
-      this.employeeSelected = employee;
-      this.$bvModal.show('editModal');
-    },
+  
 
-    onRowSelected(items) {
-      this.selected = items
-    },
-    clearSelected() {
-      this.$refs.selectableTable.clearSelected()
-    },
-    deleteEmployees(){
-      this.selected.forEach(employee => this.deleteEmployee(employee.id)) 
-    },
-    createEmployee(){
-      const url = 'http://localhost:3000/employees/addEmployees'
-      fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(this.employeeSelected),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(response => response.json())
-      .then(data => {
-        if(data.response){
-          this.getEmployees();
-          this.$bvModal.hide('editModal');
-        }
-      })
-      .catch(error => error);
-    },
-    updateEmployee(){
-      const url = `http://localhost:3000/employees/updateEmployeesData/${this.employeeSelected.id}`
-      fetch(url, {
-        method: 'PUT',
-        body: JSON.stringify(this.employeeSelected),
-        headers: { 'Content-Type': 'application/json'}
-      })
-      .then(() => this.$bvModal.hide('editModal'))
-      .catch(error => error);
-    },
-    deleteEmployee(id){
-      const url = `http://localhost:3000/employees/deleteEmployeesData/${id}`
-      fetch(url, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(() => this.getEmployees())
-      .catch(error => error);
-    }
+
+ 
+
+
+
+
+
+  methods: {
+  
+
+
+ 
+ 
+
+
+ 
+
+  // Eliminacion
+  showMsgBoxOne() {
+  this.boxOne = ''
+  this.$bvModal.msgBoxConfirm('¿Está seguro que desea eliminar los registros?')
+    .then(value => {
+      this.boxOne = value
+      const isSelected = this.selected.map(e=>e.id).length
+
+      value === true && isSelected > 0 ? 
+      (this.deleteEmployees() , this.$bvModal.msgBoxOk('Registro Eliminado Exitosamente')):
+      isSelected == 0 ? this.$bvModal.msgBoxOk('Debe seleccionar al menos 1 registro para eliminar'):
+      this.$bvModal.msgBoxOk('Operación Cancelada')
+    })
+    .catch(err => {err});
+},
+  
+  
+  getEmployees() {
+    const url = "http://localhost:3000/employees/employeesData";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => (this.employees = data.response))
+      .catch((error) => error);
   },
-}
+  showEdit(employee, mode = "edit") {
+    this.modalMode = mode;
+    this.employeeSelected = employee;
+    this.$bvModal.show("editModal");
+  },
+
+  onRowSelected(items) {
+    this.selected = items;
+    
+    
+  },
+
+  clearSelected() {
+    this.$refs.selectableTable.clearSelected();
+  },
+  deleteEmployees() {
+    this.selected.forEach((employee) => this.deleteEmployee(employee.id));
+  },
+  
+  disableSubmit () {
+    const firstname = this.employeeSelected.firstname
+    const lastname = this.employeeSelected.lastname
+    const email = this.employeeSelected.email
+    const role = this.employeeSelected.role_id
+    const pass = this.employeeSelected.user_pass
+    const passrep = this.employeeSelected.user_passrep
+    const emailRules = /.+@.+/.test(email);
+    return  !firstname || !lastname || !email || !role ||  pass !== passrep || pass.length < 6 || emailRules === false
+  },
+  
+  createEmployee() {
+    if (!this.disableSubmit()) {
+      const url = "http://localhost:3000/employees/addEmployees";
+      fetch(url, {
+        method: "POST",
+        body: JSON.stringify(this.employeeSelected),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+
+            this.getEmployees();
+            this.$bvModal.hide("editModal");
+        })
+        .catch((error) => error);
+    }else{
+      
+     
+      
+      this.$bvModal.msgBoxOk('Alguno de los formatos requeridos no son válidos o existen campos vacíos')
+    }
+    
+  },
+  updateEmployee() {
+    
+    if (!this.disableSubmit()) {
+          const url = `http://localhost:3000/employees/updateEmployeesData/${this.employeeSelected.id}`;
+          fetch(url, {
+            method: "PUT",
+            body: JSON.stringify(this.employeeSelected),
+            headers: { "Content-Type": "application/json" },
+          })
+            .then(() => {
+              this.$bvModal.hide("editModal")
+              this.getEmployees()
+              })
+            .catch((error) => error);
+          }
+    else{
+      this.$bvModal.msgBoxOk('Alguno de los formatos requeridos no son válidos o existen campos vacíos')
+    }  
+
+  },
+
+
+  deleteEmployee(id) {
+    const url = `http://localhost:3000/employees/deleteEmployeesData/${id}`;
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(() => this.getEmployees())
+      .catch((error) => error);
+  },
+},
+};
 </script>
+
+
 <style scoped>
+
 h1 {
-  font-size: 1.5em;
-  color: rgb(255, 0, 0);
-  margin-bottom: 0;
+  color: #0650c0;
+  border-color: black;
+  font-family: system-ui;
+  text-shadow: 7px 7px #dbecf1;
+  margin: 25px 0 5px 0;
+  padding: 0 0 0 1%;
+};
+
+.btnstop{
+margin-bottom: 10px;
 }
+
+.btnppal{
+  margin: 15px 0 15px 0;
+  padding: 1%;
+  font-weight: bold;
+  margin-right: 1%;
+}
+
+
 </style>
